41aaf19466ab32c61a84cea1f4b9217b1e877358
Jinesh Francis <jineshfofficial@gmail.com>
2017-07-26 18:03:40 +0530

    fixed issue #3

--- captchaimageviewlib/src/main/java/test/jinesh/captchaimageviewlib/CaptchaImageView.java
+++ captchaimageviewlib/src/main/java/test/jinesh/captchaimageviewlib/CaptchaImageView.java
@@ -41,10 +41,51 @@ public class CaptchaImageView extends ImageView {
     }
 
     @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
-        width = getMeasuredWidth();
-        height = getMeasuredHeight();
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int desiredWidth = 300;
+        int desiredHeight = 50;
+
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int width;
+        int height;
+
+        //Measure Width
+        if (widthMode == MeasureSpec.EXACTLY) {
+            //Must be this size
+            width = widthSize;
+        } else if (widthMode == MeasureSpec.AT_MOST) {
+            //Can't be bigger than...
+            width = Math.min(desiredWidth, widthSize);
+        } else {
+            //Be whatever you want
+            width = desiredWidth;
+        }
+
+        //Measure Height
+        if (heightMode == MeasureSpec.EXACTLY) {
+            //Must be this size
+            height = heightSize;
+        } else if (heightMode == MeasureSpec.AT_MOST) {
+            //Can't be bigger than...
+            height = Math.min(desiredHeight, heightSize);
+        } else {
+            //Be whatever you want
+            height = desiredHeight;
+        }
+
+        //MUST CALL THIS
+        setMeasuredDimension(width, height);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        width = w;
+        height = h;
     }
 
     /**
@@ -97,8 +138,14 @@ public class CaptchaImageView extends ImageView {
      * Redraws the captcha
      */
     private void reDraw() {
-        draw(width, height);
-        setImageBitmap(generatedCaptcha.getBitmap());
+        post(new Runnable() {
+            @Override
+            public void run() {
+                draw(width, height);
+                setImageBitmap(generatedCaptcha.getBitmap());
+            }
+        });
+
     }
 
     /**
